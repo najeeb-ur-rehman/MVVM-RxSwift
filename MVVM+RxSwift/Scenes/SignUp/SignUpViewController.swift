03322df99b4081174b56d1e27a43ffd569e1d32d
Najeeb VenD <najeeburrehman@venturedive.com>
2021-10-10 15:55:24 +0500

    Designed congratulations screen and open it on button tap

--- MVVM+RxSwift/Scenes/SignUp/SignUpViewController.swift
+++ MVVM+RxSwift/Scenes/SignUp/SignUpViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+	// MARK: Instance Properties
+	
+	var viewModel = SignUpViewModel()
+	
+	// MARK: Outlets
+	
+	@IBOutlet var signupView: SignUpView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
 	}
 
 
+	
 }
 
